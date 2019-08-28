484b7f66412f82eeba9eea28531fa8237af06c1e
Jacob Nesbitt <jjnesbitt2@gmail.com>
2019-08-28 18:25:35 -0400

    Update vscode module list and install

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,31 +1,21 @@
 #!/bin/sh
+MODULES_FILE="./modules.txt"
+
 if test "$(which code)"; then
 	if [ "$(uname -s)" = "Darwin" ]; then
 		VSCODE_HOME="$HOME/Library/Application Support/Code"
 	else
 		VSCODE_HOME="$HOME/.config/Code"
 	fi
-	mkdir -p "$VSCODE_HOME/User"
+    USER_SETTINGS="$VSCODE_HOME/User"
+	mkdir -p $USER_SETTINGS
 
-	ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
-	ln -sf "$DOTFILES/vscode/keybindings.json" "$VSCODE_HOME/User/keybindings.json"
-	ln -sf "$DOTFILES/vscode/snippets" "$VSCODE_HOME/User/snippets"
+	ln -sf "$DOTFILES/vscode/settings.json" "$USER_SETTINGS/settings.json"
+	ln -sf "$DOTFILES/vscode/keybindings.json" "$USER_SETTINGS/keybindings.json"
 
-	# from `code --list-extensions`
-	modules="
-2gua.rainbow-brackets
-blanu.vscode-styled-jsx
-dbaeumer.vscode-eslint
-dsznajder.es7-react-js-snippets
-Equinusocio.vsc-material-theme
-formulahendry.auto-close-tag
-ms-python.python
-PKief.material-icon-theme
-robertohuertasm.vscode-icons
-teabyii.ayu
-vangware.dark-plus-material
-"
-	for module in $modules; do
+
+    while IFS= read -r module
+    do
 		code --install-extension "$module" || true
-	done
+    done < $MODULES_FILE
 fi
