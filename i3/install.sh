9315a5b7f11194b83de2b2c0229a8a6674cf55e8
Jacob Nesbitt <jjnesbitt2@gmail.com>
2019-08-28 18:36:59 -0400

    Update i3 install script

--- i3/install.sh
+++ i3/install.sh
@@ -1,7 +1,11 @@
-if [ ! -d "$HOME/.config/i3" ]; then
-    sudo mkdir $HOME/.config/i3
+SYSTEM_I3_DIR="$HOME/.config/i3"
+I3_DIR="$DOTFILES/i3"
+
+
+if [ ! -d $SYSTEM_I3_DIR ]; then
+    sudo mkdir $SYSTEM_I3_DIR
 fi
 
-sudo ln -s $(pwd -P)/config $HOME/.config/i3/config
-sudo ln -s $(pwd -P)/i3blocks.conf $HOME/.config/i3/i3blocks.conf
-sudo ln -s $(pwd -P)/scripts $HOME/.config/i3/scripts
+sudo ln -s $I3_DIR/config $SYSTEM_I3_DIR/config
+sudo ln -s $I3_DIR/i3blocks.conf $SYSTEM_I3_DIR/i3blocks.conf
+sudo ln -s $I3_DIR/scripts $SYSTEM_I3_DIR/scripts
