c1fb01e86cf6b72c1ba8897da3bf7e56d3f383a4
Jacob Nesbitt <jjnesbitt2@gmail.com>
2019-08-30 13:35:18 -0400

    Fix git installing

--- git/install.sh
+++ git/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo ln -s $DOTFILES/git/gitconfig.local.symlink $HOME/.gitconfig.local
+sudo ln -s $DOTFILES/git/gitconfig.local.symlink $HOME/.gitconfig
 sudo ln -s $DOTFILES/git/gitignore.symlink $HOME/.gitignore
